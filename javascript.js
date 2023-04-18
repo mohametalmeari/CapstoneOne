4193f4b21ee1d49c8f0c45434cf47b7d44864c27
mohametalmeari <mohamet.almeari@gmail.com>
2023-04-18 23:37:43 +0300

    Fix ESLint Errors

--- javascript.js
+++ javascript.js
@@ -1,28 +1,26 @@
 const artists = [
-    {
-        name : "Tailer Swift1",
-        description :"A great American singer1",
-        details: "She is known for her loud songs1",
-        image_scr : "./images/artist1.png",
-        image_alt : "Tailer Swift Image"
-    },
-    {
-        name : "Tailer Swift2",
-        description :"A great American singer2",
-        details: "She is known for her loud songs2",
-        image_scr : "./images/artist2.png",
-        image_alt : "Tailer Swift Image2"
-    },
-    {
-        name : "Tailer Swift",
-        description :"A great American singer",
-        details: "She is known for her loud songs",
-        image_scr : "./images/artist1.png",
-        image_alt : "Tailer Swift Image"
-    }
+  {
+    name: 'Tailer Swift1',
+    description: 'A great American singer1',
+    details: 'She is known for her loud songs1',
+    image_scr: './images/artist1.png',
+    image_alt: 'Tailer Swift Image',
+  },
+  {
+    name: 'Tailer Swift2',
+    description: 'A great American singer2',
+    details: 'She is known for her loud songs2',
+    image_scr: './images/artist2.png',
+    image_alt: 'Tailer Swift Image2',
+  },
+  {
+    name: 'Tailer Swift',
+    description: 'A great American singer',
+    details: 'She is known for her loud songs',
+    image_scr: './images/artist1.png',
+    image_alt: 'Tailer Swift Image',
+  },
 ];
-
-
 
 const artistsContainer = document.getElementById('artists-container');
 for (let i = 0; i < artists.length; i += 1) {
@@ -41,6 +39,6 @@ for (let i = 0; i < artists.length; i += 1) {
       <p>${artists[i].details}</p>
   </div>
   `;
-  artistCard.classList.add("artist-card");
+  artistCard.classList.add('artist-card');
   artistsContainer.appendChild(artistCard);
 }
